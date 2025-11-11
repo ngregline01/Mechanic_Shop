091ad1a12515fc2bfbb474dd733a9a0b224da1b6
gee <ngregline@gmail.com>
2025-11-10 20:56:04 -0500

    Adds mechanic shop

---
+++ mechanics_shop.py
@@ -0,0 +1,8 @@
+from Mechanic_App.blueprint import create_app
+from Mechanic_App.blueprint.models import db
+
+mechanics_shop = create_app('DevelopmentConfig')
+with mechanics_shop.app_context():
+    db.create_all()
+
+mechanics_shop.run()
